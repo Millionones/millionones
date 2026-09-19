b3a4d2898d1fda7d50ab31e0bbb35f59f0ba765c
swalihhaneef <swalihsr7l@gmail.com>
2026-09-19 21:26:59 +0530

    superbase added

--- config.js
+++ config.js
@@ -12,6 +12,8 @@ const env = fs.existsSync(".env.development") ? ".env.development" : ".env";
 config({ path: env });
 
 export const DATABASE_URL = process.env.DATABASE_URL;
+export const SUPABASE_URL = process.env.SUPABASE_URL;
+export const SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
 if (!DATABASE_URL) {
   throw new Error("DATABASE_URL must be defined");
